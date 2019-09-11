ef798dbf6bd7937f82f7ed475306d52be97e42bb
Heejae Han <hhan1194@gmail.com>
2019-09-11 05:50:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(snacks)
-  number = snacks.index("cheddar" || "gouda" || "camembert")
+  snacks.find ("cheddar" || "gouda" || "camembert")
   snacks[number] 
 end
