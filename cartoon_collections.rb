86aaa97cc3e830e496ee59445ac93e92b8dddcce
Heejae Han <hhan1194@gmail.com>
2019-09-11 05:52:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(snacks)
-  snacks.find {|snack| snack == "cheddar" || snack == "gouda" || snack == "camembert")
+  snacks.find {|snack| snack == "cheddar" || snack == "gouda" || snack == "camembert"}
 end
